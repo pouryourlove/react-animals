e80221e78c0a0a11098240bd1716d419d1e97441
Juyeon Lee <lqu134@naver.com>
2023-02-03 21:46:12 +0900

    different way of callback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 function App() {
-  const handleClick = () => {
-    console.log("Button was clicked!");
-  };
+  // const handleClick = () => {
+  //   console.log("Button was clicked!");
+  // };
   return (
     <div>
-      <button onClick={handleClick}>Add Animal</button>
+      <button onClick={() => console.log("Button was clicked")}>
+        Add Animal
+      </button>
     </div>
   );
 }
